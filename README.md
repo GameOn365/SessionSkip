8adb0e88ac974f29176ea1a170ea8c84eb32fea9
NickG365 <nick@stormjive.com>
2013-10-14 02:49:04 -0400

    Update readme, bug fixes for commands.

--- README.md
+++ README.md
@@ -1,8 +1,31 @@
 SessionSkip
 -----------
+### Warning
+SessionSkip can allow players to log in as your staff if you do not configure it properly.  Do not use this plugin if you aren't 100% certain of what you're doing.
+
+### Description
 SessionSkip is a simple BungeeCord plugin to skip authentication with the Minecraft session servers under certain conditions.
 
 Please note: The plugin will not auto-generate the config file.  Create a plugins/SessionSkip folder and manually copy it before use.
+
+### Configuration
+SessionSkip allows players to skip authentication with the Mojang session servers when they meet one of the following configurable criterion:
+* They connect to a specific listener/hostname combination.
+* They connect using a specific hostname.
+* They connect from a specific IP address.
+
+While hostnames and IP addresses are fairly self explanatory, a listener takes two forms: A connection made directly to an IP address and port, or a connection made to an IP address and port with a specified hostname.  With that in mind, a listener could look like either of the following, depending on what the player types in when connecting:
+* If the player types "172.16.0.1"
+  * /172.16.0.1:25565
+* If the player types "play.example.com"
+  * play.example.com/172.16.0.1:25565
+
+### Commands
+* /sessionskip reload - Reloads the plugin and config file.
+* /sessionskip enable - Enables SessionSkip until BungeeCord is restarted.
+* /sessionskip disable - Disables SessionSkip until BungeeCord is restarted.
+
+SessionSkip has a "enabled" setting in the config file, which will determine whether it is enabled (true) or disabled (false) by default when BungeeCord starts up.
 
 ### Requirements
 * BungeeCord build 691+
--- src/net/gameon365/mc/plugins/bungee/sessionskip/SessionSkipCommand.java
+++ src/net/gameon365/mc/plugins/bungee/sessionskip/SessionSkipCommand.java
@@ -12,35 +12,69 @@ public class SessionSkipCommand extends Command
     public SessionSkipCommand( SessionSkip plugin )
     {
         super( "sessionskip", "sessionskip.admin" );
+        this.plugin = plugin;
+    }
+    
+    protected void sendMessage( Object p, String m )
+    {
+        if( p instanceof ProxiedPlayer )
+        {
+            ProxiedPlayer pl = (ProxiedPlayer) p;
+            pl.sendMessage( m );
+        }
+        else
+        {
+            this.plugin.getLogger().info( m );
+        }
     }
     
     public void execute( CommandSender sender, String[] args )
     {
-        ProxiedPlayer p = (ProxiedPlayer) sender;
-        switch( args[0] )
+        String name;
+        ProxiedPlayer p;
+        
+        try
         {
-            case "reload":
-                this.plugin.onDisable();
-                this.plugin.onEnable();
-                p.sendMessage( "[SessionSkip] Plugin reloaded." );
-                this.plugin.getLogger().log( Level.INFO, "[SessionSkip] Plugin reloaded by {0}.", p.getDisplayName());
-                break;
-            case "disable":
-                this.plugin.enabled = false;
-                p.sendMessage( "[SessionSkip] Plugin disabled.  WARNING: The plugin will use the value in the config file to determine whether it is enabled or disabled after the next BungeeCord restart." );
-                this.plugin.getLogger().log( Level.INFO, "[SessionSkip] Plugin disabled by {0}.  WARNING: The plugin will use the value in the config file to determine whether it is enabled or disabled after the next BungeeCord restart.", p.getDisplayName());
-                break;
-            case "enable":
-                this.plugin.enabled = true;
-                p.sendMessage( "[SessionSkip] Plugin enabled.  WARNING: The plugin will use the value in the config file to determine whether it is enabled or disabled after the next BungeeCord restart." );
-                this.plugin.getLogger().log( Level.INFO, "[SessionSkip] Plugin enabled by {0}.  WARNING: The plugin will use the value in the config file to determine whether it is enabled or disabled after the next BungeeCord restart.", p.getDisplayName());
-                break;
-            default:
-                p.sendMessages( "[SessionSkip] Commands:",
-                                "[SessionSkip] /sessionskip reload - Reload the plugin and configuration file.",
-                                "[SessionSkip] /sessionskip enable - Enable skipping authentication for rules defined in the configuration.",
-                                "[SessionSkip] /sessionskip disable - Disables skipping authentication." );
-                break;
+            p = (ProxiedPlayer) sender;
+            name = p.getDisplayName();
+        }
+        catch( ClassCastException e )
+        {
+            name = "*CONSOLE*";
+            p = null;
+        }
+        
+        try{
+            switch( args[0] )
+            {
+                case "reload":
+                    this.plugin.onDisable();
+                    this.plugin.reloadConfig();
+                    this.plugin.onEnable();
+                    this.sendMessage( p, "[SessionSkip] Plugin reloaded." );
+                    this.plugin.getLogger().log( Level.INFO, "[SessionSkip] Plugin reloaded by {0}.", name );
+                    break;
+                case "disable":
+                    this.plugin.enabled = false;
+                    this.sendMessage( p, "[SessionSkip] Plugin disabled.  WARNING: The plugin will use the value in the config file to determine whether it is enabled or disabled after the next BungeeCord restart." );
+                    this.plugin.getLogger().log( Level.INFO, "[SessionSkip] Plugin disabled by {0}.  WARNING: The plugin will use the value in the config file to determine whether it is enabled or disabled after the next BungeeCord restart.", name );
+                    break;
+                case "enable":
+                    this.plugin.enabled = true;
+                    this.sendMessage( p, "[SessionSkip] Plugin enabled.  WARNING: The plugin will use the value in the config file to determine whether it is enabled or disabled after the next BungeeCord restart." );
+                    this.plugin.getLogger().log( Level.INFO, "[SessionSkip] Plugin enabled by {0}.  WARNING: The plugin will use the value in the config file to determine whether it is enabled or disabled after the next BungeeCord restart.", name );
+                    break;
+                default:
+                    this.sendMessage( p, "[SessionSkip] Whatever you just tried to do, well, uh, that command doesn't exist." );
+                    break;
+            }
+        }
+        catch( ArrayIndexOutOfBoundsException e )
+        {
+                this.sendMessage( p, "[SessionSkip] Commands:" );
+                this.sendMessage( p, "[SessionSkip] /sessionskip reload - Reload the plugin and configuration file." );
+                this.sendMessage( p, "[SessionSkip] /sessionskip enable - Enable skipping authentication for rules defined in the configuration." );
+                this.sendMessage( p, "[SessionSkip] /sessionskip disable - Disables skipping authentication." );
         }
     }
 }
